f929182c932a41d376cc973f1717f44af6d260c4
芒针 <1514404204@qq.com>
2026-09-19 23:32:04 +0800

    hahaha
    今天还好，不是很累

---
+++ akuya.c
@@ -0,0 +1,11 @@
+﻿//来看看char!
+#include<stdio.h>
+
+int main()
+{
+    //字符跟数字相互转换时会查询ASCII码表
+    printf("a = %d\n",'a');
+    printf("0 = %c\n",0);//0其实对应空字符
+    //有点短了
+    return 0;
+}
---
+++ megumin.c
@@ -0,0 +1,27 @@
+﻿//强制转换
+//正好相反！取值范围大的，赋值给小的（此时不能直接赋值）
+#include<stdio.h>
+
+int main()
+{
+    
+    int b = 10;
+    short i = (short)b;//也就是去掉了前面两个字节
+    short f = 30;
+    int c = 65536;
+    short e = (short)c;
+    /*
+    注意啊，数据在存储时是二进制，
+    故此时short刚好容不下这个数了，会输出0
+    0000 0000 0000 0001 0000 0000 0000 0000
+    前面四组0全没了!
+    */
+    short r = (short)(i + f);
+    printf("%d\n",e);
+    
+    printf("r = %d\n",r);
+    printf("(short)(i + f) = %Iu\n",sizeof(r));
+    //%u不是很严谨的情况下，可以替代一下%Iu,%zu,
+    //因为在本机，他们的字节数(4)是一样的，而且都是无符号整数
+    return 0;
+}
